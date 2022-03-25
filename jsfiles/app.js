d2f2c85e3dbde026d8f9cb94d2fc3a51e9b1bd62
PiusAsh <Pius1ash@gmail.com>
2022-03-25 11:12:23 +0100

    Added the conditional statement for the user ID input

--- jsfiles/app.js
+++ jsfiles/app.js
@@ -1,23 +1,19 @@
 function accountDetails() {
+    let userIdInput = document.forms["myForm"]["userIdText"].value;
     let userAcctInput = document.forms["myForm"]["acctInput"].value;
-    if (userAcctInput.length === 10) {
+
+    if (userAcctInput.length === 10  && userIdInput !=='')  {
         window.localStorage.setItem('myAccountVal', userAcctInput);
+         window.localStorage.setItem('valInput', userIdInput);
 
         window.location.href = "dashboard.html";
     } else {
-        alert("Incorrect Account Number, Please try again.")
+        alert("Please enter your credientials correctly.")
     }
+    
 }
 
-function accountDetails(){
-    let userIdTextInput = document.forms["myForm"]["userIdText"].value;
-    if (userIdTextInput.length > 0) {
-      window.localStorage.setItem('myAccountUser', userIdTextInput);
-      
-      window.location.href = "dashboard.html";
-    } else {
-        alert("Please enter your username")
-    }
-}
+
+
 
 
