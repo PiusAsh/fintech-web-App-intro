c8fcdcdadf752b20fef62e8f5b10c83b35125f41
PiusAsh <Pius1ash@gmail.com>
2022-03-30 17:49:56 +0100

    Added Error message

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 function accountDetails() {
     let userIdInput = document.forms["myForm"]["userIdText"].value;
     let userAcctInput = document.forms["myForm"]["acctInput"].value;
+    const aError = document.querySelector('.accountError');
+    const uError = document.querySelector('.userError');
+    const uSuccess = document.querySelector('.userSuccess');
+    const aSuccess = document.querySelector('.accountSuccess');
 
-    if (userAcctInput.length === 10 && userIdInput !== '') {
-        window.localStorage.setItem('myAccountVal', userAcctInput);
-        window.localStorage.setItem('valInput', userIdInput);
 
-        window.location.href = "dashboard.html";
+    if (userAcctInput.length !== 10) {
+        aError.textContent = "Account number must be up to 10 digits.";
     } else {
-        alert("Please enter your credientials correctly.")
+        window.localStorage.setItem('myAccountVal', userAcctInput);
+    }
+
+    if (userIdInput !== '') {
+        window.localStorage.setItem('valInput', userIdInput);
+    } else {
+        uError.textContent = "Username is required";
+    }
+    if (userAcctInput.length === 10 && userIdInput !== '') {
+        window.location.href = "dashboard.html";
     }
 
 }
