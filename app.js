358ec3d188957a12cb8834bf63f827163798ba58
PiusAsh <Pius1ash@gmail.com>
2022-03-30 16:19:48 +0100

    Deleted the js folder

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ function accountDetails() {
     let userIdInput = document.forms["myForm"]["userIdText"].value;
     let userAcctInput = document.forms["myForm"]["acctInput"].value;
 
-    if (userAcctInput.length === 10  && userIdInput !=='')  {
+    if (userAcctInput.length === 10 && userIdInput !== '') {
         window.localStorage.setItem('myAccountVal', userAcctInput);
-         window.localStorage.setItem('valInput', userIdInput);
+        window.localStorage.setItem('valInput', userIdInput);
 
         window.location.href = "dashboard.html";
     } else {
         alert("Please enter your credientials correctly.")
     }
-    
+
 }
-
-
-
-
-
